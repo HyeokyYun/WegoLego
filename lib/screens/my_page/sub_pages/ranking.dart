0be1cbc4d8cf85062fd6453f7e22d05e3da85cab
baesubin <21800339@handong.edu>
2022-02-11 12:09:30 +0900

    [sub] ranking.dart : modify ranking ui

--- lib/screens/my_page/sub_pages/ranking.dart
+++ lib/screens/my_page/sub_pages/ranking.dart
@@ -90,18 +90,18 @@ class _Ranking_PageState extends State<Ranking_Page> {
               Container(
                 width: 340.w,
                 height: 600.h,
-                decoration: BoxDecoration(
-                  color: AppColors.grey[50],
-                  borderRadius: BorderRadius.circular(22.w),
-                  boxShadow: [
-                    BoxShadow(
-                      color: AppColors.grey.withOpacity(0.1),
-                      // spreadRadius: 5,
-                      blurRadius: 20,
-                      offset: Offset(0, 5), // changes position of shadow
-                    ),
-                  ],
-                ),
+                // decoration: BoxDecoration(
+                //   color: AppColors.grey[50],
+                //   borderRadius: BorderRadius.circular(22.w),
+                //   boxShadow: [
+                //     BoxShadow(
+                //       color: AppColors.grey.withOpacity(0.1),
+                //       // spreadRadius: 5,
+                //       blurRadius: 20,
+                //       offset: Offset(0, 5), // changes position of shadow
+                //     ),
+                //   ],
+                // ),
                 child: (dropdown)
                     ? StreamBuilder<QuerySnapshot>(
                         stream: FirebaseFirestore.instance
@@ -166,67 +166,74 @@ class _Ranking_PageState extends State<Ranking_Page> {
 }
 
 Widget _buildItemWidget(DocumentSnapshot doc) {
-  final rank = Rank(doc['help'], doc['ask'], doc['name']);
+  final rank = Rank(doc['help'], doc['ask'], doc['name'],doc['photoURL']);
 
-  //var num = ['1','2','3','4','5','6','7','8','9','10'];
   return ListTile(
       title: Container(
-    color: AppColors.grey[50],
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                width: 200.w,
-                child: Row(children: [
-                  //Text("$num[index]"),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(57),
-                    child: SvgPicture.asset(
-                      "assets/liveQ_logo.svg",
-                      height: 30.h,
-                      width: 30.w,
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 10.w,
-                  ),
-                  Text(
-                    rank.name,
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ]),
-              ),
-              Container(
-                  height: 15,
-                  child: Row(
-                    children: [
-                      SizedBox(
-                          width: 65.w,
-                          child: Row(children: [
-                            SvgPicture.asset("assets/my_page/heartIcon.svg"),
-                            Text(
-                              "${rank.ask}",
-                              style: TextStyle(fontSize: 16),
-                            ),
-                          ])),
-                      Image.asset("assets/my_page/yellow_i.png"),
-                      SizedBox(width: 7.w),
-                      Text(
-                        "${rank.help}",
-                        style: TextStyle(fontSize: 16),
+        //color: AppColors.grey[50],
+        height: 60.h,
+        child: Card(
+          shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(11)
+          ),
+        color: Color(0xffFFF9EA),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Container(
+                  width: 200.w,
+                  child: Row(children: [
+                    //Text("$num[index]"),
+
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(57),
+                      child: Image.network(
+                        rank.photo,
+                        height: 30.h,
+                        width: 30.w,
+                        fit: BoxFit.fill,
                       ),
-                    ],
-                  )),
-            ]),
-        Divider(
-          color: AppColors.grey[400],
-          // thickness: 1.0,
-        ),
-      ],
+                    ),
+                    SizedBox(
+                      width: 10.w,
+                    ),
+                    Container(
+                      width: 130.w,
+                      child: Text(
+                        rank.name,
+                        style: TextStyle(fontSize: 15.w),
+                      ),
+                    ),
+                  ]),
+                ),
+                Container(
+                    height: 15,
+                    child: Row(
+                      children: [
+                        SizedBox(
+                            width: 65.w,
+                            child: Row(children: [
+                              SvgPicture.asset("assets/my_page/heartIcon.svg"),
+                              Text(
+                                "${rank.ask}",
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ])),
+                        Image.asset("assets/my_page/yellow_i.png"),
+                        SizedBox(width: 7.w),
+                        Text(
+                          "${rank.help}",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    )),
+              ]),
+
+        ],
+      ),
     ),
   ));
 }
@@ -235,6 +242,10 @@ class Rank {
   int help;
   int ask;
   String name;
+  String photo;
 
-  Rank(this.help, this.ask, this.name);
+  Rank(this.help, this.ask, this.name, this.photo);
+
+
+
 }
