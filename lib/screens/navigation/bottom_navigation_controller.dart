4a8c0efb5515c733fc27ab13104f40e2f7c9e6bd
HyeokyYun <hyeoky98@gmail.com>
2022-02-11 13:06:42 +0900

    [HY] - Navigation => BottomNavigation으로 이름 수정

--- lib/screens/navigation/bottom_navigation_controller.dart
+++ lib/screens/navigation/bottom_navigation_controller.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class BottomNavigationController extends GetxController {
-  var tabIndex = 1.obs;
+  var tabIndex = 1.obs; // Navigation의 List 인덱스 번호.
 
   void changeTabIndex(int index) {
     tabIndex.value = index;
