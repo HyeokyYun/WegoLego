2184e25d0eef1a33884ed1732abb90b15613f939
RyuhaJeffery <yws2121@naver.com>
2022-02-14 11:12:52 +0900

    [ryu] git hub test

--- lib/screens/my_page/sub_pages/thanks_letters.dart
+++ lib/screens/my_page/sub_pages/thanks_letters.dart
@@ -11,6 +11,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/src/extension_navigation.dart';
+//ryuha jeffery test
 
 class ThankyouLetters extends StatefulWidget {
   const ThankyouLetters({Key? key}) : super(key: key);
